5a1809b504d0042115bb036d5a83429ef3521c3e
Trung Nam <trungnam.work@gmail.com>
2024-11-25 01:06:53 +0700

    logout

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,16 @@ const registerUser = require('../controllers/registerUser');
 const checkEmail = require('../controllers/checkEmail');
 const checkPassword = require('../controllers/checkPassword');
 const userDetails = require('../controllers/userDetails');
+const logout = require('../controllers/logout');
 
 
 const router = express.Router()
 
 // Create user api
-router.post('/register',registerUser)
-router.post('/email',checkEmail)
-router.post('/password',checkPassword)
-router.get('/user-details',userDetails)
+router.post('/register', registerUser)
+router.post('/email', checkEmail)
+router.post('/password', checkPassword)
+router.get('/user-details', userDetails)
+router.get('/logout', logout)
 
 module.exports = router;
