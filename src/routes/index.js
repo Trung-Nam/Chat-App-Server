32b262f8f0365db706223bfd1bf5b3d86e431f38
Trung Nam <trungnam.work@gmail.com>
2024-11-25 00:46:07 +0700

    Add jwt verify password

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const registerUser = require('../controllers/registerUser');
 const checkEmail = require('../controllers/checkEmail');
+const checkPassword = require('../controllers/checkPassword');
 
 
 const router = express.Router()
@@ -8,5 +9,6 @@ const router = express.Router()
 // Create user api
 router.post('/register',registerUser)
 router.post('/email',checkEmail)
+router.post('/password',checkPassword)
 
 module.exports = router;
