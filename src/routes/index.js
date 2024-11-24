77403ce5d61e14c1a185512b3c4008557c6d6351
Trung Nam <trungnam.work@gmail.com>
2024-11-25 01:15:53 +0700

    Update user

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,17 @@ const checkEmail = require('../controllers/checkEmail');
 const checkPassword = require('../controllers/checkPassword');
 const userDetails = require('../controllers/userDetails');
 const logout = require('../controllers/logout');
+const updateUserDetails = require('../controllers/updateUserDetails');
 
 
 const router = express.Router()
 
-// Create user api
+
 router.post('/register', registerUser)
 router.post('/email', checkEmail)
 router.post('/password', checkPassword)
 router.get('/user-details', userDetails)
 router.get('/logout', logout)
+router.post('/update-user', updateUserDetails)
 
 module.exports = router;
