fc75d2f00400ffd83b2cd65e040a8b81c5634947
Trung Nam <trungnam.work@gmail.com>
2024-11-25 00:06:35 +0700

    Connect db

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express')
-const cors = require('cors');
-const connectDB = require('./config/mongodb');
-require('dotenv').config();
-
-
+const cors = require('cors')
+const connectDB = require('./src/config/connectDB')
+const router = require('./src/routes/index')
 const app = express();
+require('dotenv').config()
 
+// const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
 
-const PORT = process.env.PORT || 8080;
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Example app listening on http://localhost:${PORT}`);
-    });
+const PORT = process.env.PORT || 8080
 
+app.get('/',(request,response)=>{
+    response.json({
+        message : "Server running at " + PORT
+    })
+})
+
+//api endpoints
+app.use('/api',router)
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server listening on http://localhost:${PORT}`);
+    })
 })
